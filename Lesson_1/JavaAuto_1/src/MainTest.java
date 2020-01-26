71283d813878bc4ab3ec446dfc3f06d0f8b37f28
Max_Che <51440829+Gloomy-Monk@users.noreply.github.com>
2020-01-26 18:27:44 +0300

    2sm

--- Lesson_1/JavaAuto_1/src/MainTest.java
+++ Lesson_1/JavaAuto_1/src/MainTest.java
@@ -2,25 +2,17 @@ import org.junit.Test;
 
 public class MainTest extends CoreTestCase {
 
+    public void typeStartMessage(){
+        super.typeStartMessage();
+        System.out.println("Current class MainTest");
+    }
     @Test
     public void myFirstTest() {
-        System.out.println("First test : Before changing static_int: " + MathHelper.static_int);
-
-        MathHelper.static_int = 8;
-
-        MathHelper mathObject = new MathHelper();
-        System.out.println("First test : Before changing static_int: " + mathObject.simple_int);
-        mathObject.simple_int = 8;
+        this.typeStartMessage();
     }
 
     @Test
     public void mySecondTest() {
-        System.out.println("Second test : Before changing static_int: " + MathHelper.static_int);
-
-        MathHelper.static_int = 8;
-
-        MathHelper mathObject = new MathHelper();
-        System.out.println("Second test : Before changing static_int: " + mathObject.simple_int);
-        mathObject.simple_int = 8;
+        this.typeStartMessage();
     }
 }
